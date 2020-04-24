6809cc0dcdaf0003dc9bc9f12e718819755e4e2f
Jake Chustz <jake.chustz@gmail.com>
2020-04-24 12:32:39 -0700

    started skeleton build and add images to assets folder. started playing around with adding images

---
+++ assets/js/script.js
@@ -0,0 +1,131 @@
+var gameCards = document.querySelector("#gameCards");
+var firstCardClicked;
+var secondCardClicked;
+var firstCardClasses;
+var secondCardClasses;
+var maxMatches = 9;
+var matches = 0;
+var modalEl = document.querySelector("#modal");
+var attempts = 0;
+var gamesPlayed = 0;
+var accuracyText = document.querySelector("#accuracyNum");
+var attemptsText = document.querySelector("#attemptsNum");
+var gamesPlayedText = document.querySelector("#gamesNum");
+var modalButton = document.getElementById("modal-button");
+var logoClasses = ["css-logo", "docker-logo", "github-logo", "html-logo", "js-logo", "mysql-logo", "node-logo", "php-logo", "react-logo", "css-logo", "docker-logo", "github-logo", "html-logo", "js-logo", "mysql-logo", "node-logo", "php-logo", "react-logo"];
+var cardFrontEl = document.getElementsByClassName("card-front");
+var cardBack = document.getElementsByClassName("card-back");
+
+
+gameCards.addEventListener("click", handleClick);
+
+
+function handleClick(event) {
+  if (event.target.className.indexOf("card-back") === -1) {
+    return;
+  }
+  event.target.className += " hidden";
+  if (!firstCardClicked) {
+    firstCardClicked = event.target
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  } else {
+    secondCardClicked = event.target
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    gameCards.removeEventListener("click", handleClick);
+    if (firstCardClasses === secondCardClasses) {
+      firstCardClicked = null;
+      secondCardClicked = null;
+      gameCards.addEventListener("click", handleClick)
+      matches++;
+      attempts++;
+      if (matches === maxMatches) {
+        modalEl.classList.remove("hidden");
+      }
+      //displayStats();
+    } else {
+      setTimeout(function () {
+        firstCardClicked.classList.remove("hidden");
+        secondCardClicked.classList.remove("hidden");
+        firstCardClicked = null;
+        secondCardClicked = null;
+        gameCards.addEventListener("click", handleClick);
+      }, 1000)
+      attempts++;
+      //displayStats();
+    }
+  }
+}
+
+/*
+function displayStats() {
+  document.querySelector("#gamesNum").textContent = gamesPlayed;
+  document.querySelector("#attemptsNum").textContent = attempts;
+  document.querySelector("#accuracyNum").textContent = (calculateAccuracy(attempts, matches));
+}
+
+function calculateAccuracy(attempts, matches) {
+  return Math.trunc((matches / attempts) * 100) + "%";
+}
+
+
+
+function resetGame() {
+  attempts = 0;
+  attemptsText.textContent = 0;
+  matches = 0;
+  accuracyText.textContent = 0;
+  gamesPlayedText.textContent = gamesPlayed;
+  gamesPlayed++;
+  displayStats();
+  resetCards();
+  shuffleCards();
+  modalEl.classList.add("hidden");
+}
+
+function resetCards() {
+  var hiddenCards = document.querySelectorAll(".card-back");
+  for (var index = 0; index < hiddenCards.length; index++) {
+    hiddenCards[index].classList.remove("hidden");
+  }
+}
+modalButton.addEventListener("click", resetGame);
+
+
+function shuffleCards() {
+  for (let s = 0; s < logoClasses.length; s++) {
+    var randomSpot = Math.floor(Math.random() * logoClasses.length);
+    var finalSpot = logoClasses[s];
+    logoClasses[s] = logoClasses[randomSpot];
+    logoClasses[randomSpot] = finalSpot;
+  }
+  displayCards();
+}
+
+function displayCards() {
+  for (let d = 0; d < cardFrontEl.length; d++) {
+    cardFrontEl[d].className = "card-front " + logoClasses[d];
+  }
+}
+
+function createCards() {
+  for (var i = 0; i < logoClasses.length; i++) {
+    var div = document.createElement("div");
+    div.classList.add("card", "col-2");
+
+    var cardFront = document.createElement("div");
+    cardFront.classList.add("card-front", logoClasses[i]);
+
+    var cardBack = document.createElement("div");
+    cardBack.classList.add("card-back");
+
+    div.append(cardFront, cardBack);
+    gameCards.append(div);
+  }
+}
+
+window.addEventListener("load", load);
+function load() {
+  shuffleCards();
+  createCards();
+}
+*/
